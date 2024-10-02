aa2ce41cbba245f6b426082b32dbe5740d20822b
Phạm Văn Nghĩa <130687382+nghiaphamaudev@users.noreply.github.com>
2024-10-02 21:45:28 +0700

    Resolve conflict auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,122 +1,224 @@
-import mongoose from 'mongoose';
-import bcryptjs from 'bcryptjs';
-import crypto from 'crypto';
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      lowercase: true,
-    },
-    fullName: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    avatar: {
-      type: String,
-      default: 'default.jpg',
-    },
-    role: {
-      type: String,
-      enum: ['admin', 'user'],
-      default: 'user',
-    },
-    rank: {
-      type: String,
-      enum: ['Bronze', 'Silver', 'Gold'],
-      default: 'Bronze',
-    },
-    addresses: [
-      {
-        name: String,
-        phone: String,
-        address: String,
-        isDefault: Boolean,
-      },
-    ],
-    favoriteProduct: [
-      {
-        product: {
-          type: mongoose.Schema.ObjectId,
-          required: false,
-          ref: 'Laptop',
-        },
-      },
-    ],
-    password: {
-      type: String,
-      select: false,
-      required: true,
-    },
-    passwordChangedAt: Date,
-    passwordResetToken: String,
-    passwordResetTokenExpires: Date,
-    active: {
-      type: Boolean,
-      default: true,
-      select: true,
-    },
-  },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-    versionKey: false,
-    timestamps: true,
-  }
-);
+const crypto = require('crypto');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const Email = require('../utils/email');
+const { url } = require('inspector');
 
-//Mã hóa password trước khi lưu vào db
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcryptjs.hash(this.password, 12);
-  next();
-});
-
-//kiểm tra password có được tạo mới hay thay đổi không , nếu ko thì ko cần cập nhật passwordchangedAt
-//Nếu password thay đổi => passwordchangeAt = Thời gian hiện tại  -1000ms
-//passwordChangedAt luôn trước thời điểm token JWT được tạo, tránh tình huống xung đột do sự chênh lệch thời gian.
-userSchema.pre('save', function (next) {
-  if (!this.isModified('password') || this.isNew) return next();
-  this.passwordChangedAt = Date.now() - 1000;
-  next();
-});
-
-//So sánh password db với password nhập bởi user
-userSchema.methods.checkPassword = async function (password) {
-  return await bcryptjs.compare(password, this.password);
+const signToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 };
 
-//dùng để kiểm tra  người dùng có thay đổi pass sau khi mã jwt được tạo hay không
-//nếu this.passwordChangedAT tồn tại thì => user đã thay đổi password mà chưa đăng nhập lại
-// Nếu changedTimestamps > JWTTimestamp  => người dùng đã thay đổi pass sau khi jwt đc tạo +. người dùng dăng nhập lại
-//Nói cách khác là khi người dùng đăng nhập vào và sau đó thay đổi mật khẩu thì bắt phải đăng nhập lại
-userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-  if (this.passwordChangedAt) {
-    const changedTimestamps = parseInt(
-      this.passwordChangedAt.getTime() / 1000,
-      10
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPRIES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user: user,
+    },
+  });
+};
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
+  createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  //select lay them truong password boi vi de mac dinh khon glay truong password
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  createSendToken(user, 201, res);
+});
+
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    status: 'success',
+  });
+};
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //Kiểm tra token có thực sự đi kèm với header ko
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to access'));
+  }
+  //Giải mã token
+
+  //promisify dùng để biển đổi 1 hàm callback thành promise
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user beloging to this token does not exist.', 401)
     );
-    return JWTTimestamp < changedTimestamps;
   }
-  return false;
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again', 401)
+    );
+  }
+  req.user = currentUser;
+  res.locals.user = currentUser;
+  next();
+});
+
+//Only for rended pages, no errors
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
 };
 
-//Tạo 1 resetToken để
-//resetToken chưa hash => user
-// reset hashed => db
-userSchema.methods.createPasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(32).toString('hex');
-  this.passwordResetToken = crypto
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    //roles ['admin','lead-guid']
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+    next();
+  };
+};
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get User based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with no email address.', 404));
+  }
+  //2) Generate the random tyhe token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  //3) Sent if to user's email
+
+  try {
+    const resetUrl = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    await new Email(user, resetUrl).sendPasswordReset();
+    res.status(200).json({
+      status: 'success',
+      messager: ' Token sent to email',
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new AppError(
+        'There was an error sending the email. Try again later!',
+        500
+      )
+    );
+  }
+});
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  //1) Get use based on the token
+  const hashedToken = crypto
     .createHash('sha256')
-    .update(resetToken)
+    .update(req.params.token)
     .digest('hex');
 
-  this.passwordResetTokenExpires = Date.now() + 10 * 60 * 1000;
-  return resetToken;
-};
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
 
-const User = mongoose.model('User', userSchema);
-export default User;
+  //2) If token has not expired, and there is user, set the new Password
+  if (!user) {
+    return next(new AppError(' Token invalid or has expried ', 400));
+  }
+  //3) Update changedPasswordAt property for the user
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetExpires = undefined;
+  user.passwordResetToken = undefined;
+
+  await user.save();
+  //4) Login the user in , send JWT
+  createSendToken(user, 201, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  //Lấy từ protect
+  const currentUser = await User.findById(req.user.id).select('+password');
+  if (!currentUser) {
+    return next(new AppError('Account not existed', 400));
+  }
+  //2) Check if posted cureent password is correct
+
+  if (
+    !(await currentUser.correctPassword(
+      req.body.passwordCurrent,
+      currentUser.password
+    ))
+  ) {
+    return next(new AppError('Your current password wrong', 401));
+  }
+  //3) if so , update password
+
+  //Cach viet 2
+  currentUser.password = req.body.password;
+  currentUser.passwordConfirm = req.body.passwordConfirm;
+  await currentUser.save();
+  //4) Log user in , send jwt
+  createSendToken(currentUser, 201, res);
+});
